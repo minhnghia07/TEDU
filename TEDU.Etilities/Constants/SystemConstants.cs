e8ed55741844c49206302d84276cab8d63497a65
Nguyen Minh Nghia (Aptech HN) <nghianmth1907030@fpt.edu.vn>
2021-03-11 15:48:16 +0700

    Backend Api

---
+++ TEDU.Etilities/Constants/SystemConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEDU.Utilities.Constants
+{
+    public class SystemConstants
+    {
+        public const string MainConnectionstring = "TEDUDb";
+    }
+}
