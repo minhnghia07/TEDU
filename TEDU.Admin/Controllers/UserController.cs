29525091a8e7fff7e7a5e6182e3855bbeaa13adf
minhnghia07 <56199391+minhnghia07@users.noreply.github.com>
2021-05-25 16:53:53 +0700

    Login-Authenticate

---
+++ TEDU.Admin/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TEDU.Admin.Services;
+using TEDU.ViewModels.System.Users;
+
+namespace TEDU.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        public readonly IUserApiClient _userApiClient;
+        public UserController(IUserApiClient userApiClient)
+        {
+            _userApiClient = userApiClient;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return View(ModelState);
+
+            var token = await _userApiClient.Authenticate(request);
+            return View(token);
+        }
+    }
+}
---
+++ TEDU.Admin/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TEDU.ViewModels.System.Users;
+
+namespace TEDU.Admin.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
